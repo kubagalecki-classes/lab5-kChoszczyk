21fb2474652e2a3f610409e77cd00769cc612155
kChoszczyk <krzysiex97@gmail.com>
2020-12-07 18:24:32 +0000

    poprawiony namespace

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -11,7 +11,7 @@ std::vector< char > foo(std::list< Human >& people)
   std::vector <char> v;
   
     // Twoja implementacja tutaj
-    for (list< Human >::reverse_iterator it = people.rbegin(); it != people.rend(); ++it) {
+    for (std::list< Human >::reverse_iterator it = people.rbegin(); it != people.rend(); ++it) {
         it->birthday();
         if (it->isMonster() == true) {
             v.push_back('n');
